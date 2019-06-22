df2dc8c0e253ff50461e2850b9ca5a076690008c
Andrew Silva <andrewsilva9@gmail.com>
2019-06-22 18:49:08 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -92,17 +92,17 @@ class Game
     if game_mode == "1"
       puts "Would you like to go first? (y/n)"
       if gets.strip == "y"
-        current_player(Players::Human.new("X"), Players::Computer.new("O"), Board.new).play
+        Game.new(Players::Human.new("X"), Players::Computer.new("O"), Board.new).play
       else
-        current_player(Players::Computer.new("X"), Players::Human.new("O"), Board.new).play
+        Game.new(Players::Computer.new("X"), Players::Human.new("O"), Board.new).play
       end
 
     elsif game_mode == "2"
       puts "Would you like to go first? (y/n)"
-      current_player(Players::Human.new("X"), Players::Human.new("O"), Board.new).play
+      Game.new(Players::Human.new("X"), Players::Human.new("O"), Board.new).play
 
     elsif game_mode == "0"
-      current_player(Players::Computer.new("X"), Players::Computer.new("O"), Board.new).play
+      Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new).play
     end
     puts "Do you want to play again? (y/n)"
   end
